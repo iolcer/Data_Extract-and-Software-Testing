aa6a79851d93b6f7fb249301a3addce47c92782e
OLCER <67601171+iolcer@users.noreply.github.com>
2020-07-14 12:33:04 +0300

    Add files via upload

---
+++ Scrapy Kitapyurdu/kitapyurdu/kitapyurdu/spiders/book_spider.py
@@ -0,0 +1,43 @@
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    page_count = 0
+    book_count = 1
+    
+    file = open("books.txt","a", encoding = "utf-8")
+    start_urls = [
+            'https://www.kitapyurdu.com/index.php?route=product/best_sellers&list_id=1&filter_in_stock=1&filter_in_stock=1&page=1'
+        ]
+        
+    def parse(self, response):
+        book_names = response.css("div.name.ellipsis a span::text").extract()
+        book_authors = response.css("div.author span a span::text").extract()
+        book_publishers = response.css("div.publisher span a span::text").extract()
+
+        i = 0
+        self.file.write("------------------------------------------------------\n")
+        self.file.write( "   Kitapyurdu.com ÇOK SATAN KİTAPLAR LİSTESİ \n")
+        while (i < len(book_names)):
+         
+            
+            self.file.write("------------------------------------------------------\n")
+            self.file.write(str(self.book_count) + ". " + "Kitap İsmi: " + book_names[i] + "   " + "Yazar: " + book_authors[i] + "  " + "Yayıncı: " + book_publishers[i] + "\n")
+            self.book_count += 1
+
+            """yield {
+                "name": book_names[i],
+                "author": book_authors[i],
+                "publisher": book_publishers[i]
+            }"""
+            i += 1
+            
+        next_url = response.css("a.next::attr(href)").extract_first()
+        self.page_count +=1
+
+        if next_url is not None and self.page_count != 5:
+            yield scrapy.Request(url = next_url, callback = self.parse)
+        else:
+            self.file.close()
+
